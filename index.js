8c3bc81414f2b632b1561467d73d091f161d89f4
L245854 <l245854@pn1706924.lanl.gov>
2018-07-28 18:49:46 -0600

    Finished mini assignment

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const massive = require("massive")
+require('dotenv').config()
+const pc = require("./controller.js")
 
 const app = express();
+
+massive(process.env.CONNECTION_STRING).then(dbInstance =>{
+    app.set('db', dbInstance)
+    // dbInstance.new_planes()
+    // .then(planes => console.log(planes))
+    // .catch(err=> console.log(err))
+
+    // dbInstance.get_planes()
+    // .then(planes=>console.log(planes))
+    // .catch(err => console.log(err))
+})
+
+app.get('/api/planes/', pc.getPlanes)
+
 app.use( bodyParser.json() );
 app.use( cors() );
 
